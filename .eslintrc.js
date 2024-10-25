6d9acd585fb8e318f35ac75f80df9fbdb27f0c21
Jake Unsted <jake.unsted@gmail.com>
2024-10-25 14:36:14 +0100

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,10 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  rules: {
+    'semi': ['error', 'always'],
+    'indent': ['error', 2, { 'SwitchCase': 1 }],
+    'quotes': ['error', 'single', { 'avoidEscape': true }],
+    'max-len': ['error', { 'code': 80 }],
+  },
 };
